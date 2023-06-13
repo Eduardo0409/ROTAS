fefd6c84918630bfce8353cc268a8b3cc1f79aec
Eduardo0409 <100179167+Eduardo0409@users.noreply.github.com>
2023-06-13 00:12:35 -0300

    Navbar com rota usando routerLink

---
+++ src/app/mult-table-detals/mult-table-detals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mult-table-detals',
+  templateUrl: './mult-table-detals.component.html',
+  styleUrls: ['./mult-table-detals.component.css']
+})
+export class MultTableDetalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
